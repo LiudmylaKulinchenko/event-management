fae5a54b3df067bba5b65fb2b6a96c262950ce79
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2023-02-08 18:37:30 +0200

    Add using .env file to settings.py

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b0bpm@43)hff1fa8a)h+-5m_)!3i21ax2&#_td9@8yq0=ymfr^"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
