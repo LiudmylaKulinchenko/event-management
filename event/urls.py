9505ce64f0175d0e841f63bf05420cf2cebffca1
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2023-02-08 13:50:58 +0200

    Fix path names in urls.py event app

--- event/urls.py
+++ event/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from event.views import EventTypeView, EventView
 
 
 urlpatterns = [
-    path("events/", EventView.as_view(), name="list-event"),
-    path("event-types/", EventTypeView.as_view(), name="list-event-type"),
+    path("events/", EventView.as_view(), name="event-list"),
+    path("event-types/", EventTypeView.as_view(), name="event-type-list"),
 ]
 
 app_name = "event"
--- event/views.py
+++ event/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics
+from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
